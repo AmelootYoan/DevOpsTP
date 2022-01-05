58f88d26d7574b7704930dea3994a77cf4da73c0
AmelootYoan <75621154+AmelootYoan@users.noreply.github.com>
2022-01-05 22:57:26 +0100

    site dockerisé YES !
    je peux maintenant partir en paix...

---
+++ php/index.php
@@ -0,0 +1,41 @@
+<!-- ./php/index.php -->
+
+<html>
+    
+    <head>
+
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style.css">
+        <title>Tout le monde aime le saucisson</title>
+        <link rel="icon" href="./images/icon_saucisson.png">
+
+
+    </head>
+
+    <body>
+
+        <h1>Le saucisson</h1> 
+        <img src = "./images/saucisson.jpeg" />
+
+        <h2> Définition d'un saucisson </h2>
+        <p>Un saucisson est une charcuterie composée d'un hachis d'une ou plusieurs viandes, principalement du porc, que l'on assaisonne de multiples façons suivant des traditions locales, régionales ou nationales. Après avoir été mis dans un boyau naturel ou artificiel (embossage) pour lui donner sa forme, le saucisson est soit « étuvé et/ou séché », parfois fumé pendant au moins quatre semaines, soit « étuvé et/ou poché » puis refroidi dans un bouillon, comme pour le saucisson à l'ail. Son diamètre peut aller de quelques millimètres jusqu'à une vingtaine de centimètres pour la mortadelle. </p>    
+    
+        <h2> Histoire </h2>
+        <p>En latin, salsus signifie « salé ». On pense que les Grecs et les Romains sont à l’origine de la saucisse et du saucisson. On retrouve des recettes de saucisson 
+            datant du temps de l’Empire romain.
+            De plus, en accompagnement de la cervoise, les Gaulois consommaient des légumes secs, des fèves, des lentilles et surtout du cochon. Les salaisons connues de 
+            l’époque gallo-romaine sont le porc salé, le jambon et, notamment, le saucisson.
+            En 1546, le mot saucisson, d'origine italienne, apparaît en France dans le Tiers Livre de Rabelais. C'est un indice de la pénétration de la cuisine transalpine. </p>
+
+        <h2> Fabrication </h2>
+        <p>Le saucisson est composé d’un boyau d’animal (bœuf, porc…) fourré de viande hachée, salée ou saumurée, parfois agrémentée de fruits, légumes, de fromages ou 
+            d’alcool. La viande utilisée est généralement de la viande de porc, mais il en existe contenant aussi du taureau, de l’agneau, de l’âne, du bœuf, du cheval, 
+            du sanglier ou de la volaille.
+            Cependant, le saucisson nécessite une certaine quantité de graisse pure, qui ne peut venir quasiment que du cochon : ainsi la quasi-totalité des saucissons 
+            contiennent du porc, et ne sont que « à la viande de » taureau, âne, etc. Le « saucisson d'âne » vendu aux touristes en Corse n'a ainsi rien d'une spécialité 
+            régionale (les Corses élèvent des ânes mais n'en mangent pas), mais est un saucisson de cochon industriel contenant entre 5 et 20 % de viande d'âne ou de mule 
+            originaire d'Amérique latine.</p>
+
+    </body>
+
+</html>
